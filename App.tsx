4b41f2165c306b235835c39fd6fda018cedaf515
aenewsss <aenamartinelli@gmail.com>
2023-12-13 11:50:11 -0300

    first commit

--- App.tsx
+++ App.tsx
@@ -1,11 +1,37 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { GoogleSignin, GoogleSigninButton } from '@react-native-google-signin/google-signin';
+import auth from '@react-native-firebase/auth';
 
 export default function App() {
+  GoogleSignin.configure({
+    webClientId: '6786179990-q8o3kneocph1ca7lpslm5hlrkre6fc40.apps.googleusercontent.com',
+  })
+
+  async function onGoogleButtonPress() {
+    // Check if your device supports Google Play
+    await GoogleSignin.hasPlayServices({ showPlayServicesUpdateDialog: true });
+    // Get the users ID token
+    const { idToken, user } = await GoogleSignin.signIn();
+
+    console.log(user)
+    
+    // Create a Google credential with the token
+    const googleCredential = auth.GoogleAuthProvider.credential(idToken);
+
+    // Sign-in the user with the credential
+    return auth().signInWithCredential(googleCredential);
+  }
+
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
       <StatusBar style="auto" />
+      <GoogleSigninButton
+        size={GoogleSigninButton.Size.Wide}
+        color={GoogleSigninButton.Color.Dark}
+        onPress={onGoogleButtonPress}
+      />
     </View>
   );
 }
